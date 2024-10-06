bb7998391eb6f4a53daed10452049af935116dea
runejac <runedanielj@gmail.com>
2024-10-06 16:59:58 +0200

    nav, imports, icons, about content
    Fixed and adjusted header on mobile and desk, added icons for footer,
    some adjustments to background, deleted some unused files and folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.runeoliveira.com',
   integrations: [mdx(), sitemap(), icon()],
