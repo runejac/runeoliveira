6114b9f7366cc2380c6fe54cfd71c9a72f6ab8bb
runejac <runedanielj@gmail.com>
2024-11-01 17:57:32 +0100

    global css made into astro file and import aliases updated

--- src/consts.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_TITLE = 'Rune Oliveira';
-export const SITE_DESCRIPTION =
-  'Welcome fellow friend. I am a Software Engineer based in Oslo, Norway, focusing on user-centric development with specialization in frontend development.';
