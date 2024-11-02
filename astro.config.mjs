3ccb40dcd5944bdc1664901ad3845ef196d97c07
runejac <runedanielj@gmail.com>
2024-11-02 13:28:37 +0100

    fix MIME type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://runeoliveira.com',
   integrations: [sitemap(), icon()],
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+    // Proper TypeScript handling
+    optimizeDeps: {
+      include: ['three']
+    },
+    resolve: {
+      alias: {
+        'three/addons': 'three/examples/jsm'
+      }
+    }
+  },
 });
