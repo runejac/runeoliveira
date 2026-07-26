928e509a848b48a4581f0551b0581b5ab539c27b
Rune Oliveira <69790920+runejac@users.noreply.github.com>
2026-07-26 20:25:33 +0200

    Redesign portfolio for 2026

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
-import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://runeoliveira.com',
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap()],
   vite: {
     build: {
       assetsInlineLimit: 0,
     },
-    optimizeDeps: {
-      include: ['three']
-    },
-    resolve: {
-      alias: {
-        'three/addons': 'three/examples/jsm'
-      }
-    }
   },
 });
